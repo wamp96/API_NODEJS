636685ea6d2a53e0a81438b1bca721162c0f3f59
Willian Andres Moreno <92280108+wamp96@users.noreply.github.com>
2024-06-25 23:28:38 -0500

    ACTUALIZACION "Se actualiza products.routes.js"

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,12 +1,23 @@
 import { Router } from "express";
-const router = Router()
+import {
+    getProducts,
+    createProduct,
+    updateProductById,
+    deleteProductById,
+    getProductById,
+} from "../controllers/products.controller.js";
+import { verifyToken, isModerator, isAdmin } from "../middlewares/authJwt.js";
 
-import * as productsCtrl from "../controllers/products.controller";
+const router = Router();
 
-router.post('/', productsCtrl.createProduct);
-router.get('/', productsCtrl.getProducts);
-router.get('/:productId', productsCtrl.getProductById);
-router.put('/:productId', productsCtrl.updateProductById);
-router.delete('/:productId', productsCtrl.deleteProductById);
+router.get("/", getProducts);
+
+router.get("/:productid", getProductById);
+
+router.post("/", [verifyToken, isModerator], createProduct);
+
+router.put("/:productId", [verifyToken, isModerator], updateProductById);
+
+router.delete("/:productId", [verifyToken, isAdmin], deleteProductById);
 
 export default router;
