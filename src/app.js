59ae832a0f23b4bbadf5e83154cc6faecbc9e433
Willian Andres Moreno <92280108+wamp96@users.noreply.github.com>
2024-06-25 23:45:55 -0500

    ACTUALIZACION "se actualiza app.js"

--- src/app.js
+++ src/app.js
@@ -1,28 +1,38 @@
-import express from 'express'
-import morgan from 'morgan'
-import pkg from '../package.json'
-import productsRoutes from './routes/products.routes'
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
 
-const app = express()
-
-
-app.set('pkg', pkg);
-
-//Middleware
-app.use(express.json())
-app.use(morgan('dev'));
 
 //Routes
-app.get('/', (req, res) => {
-  res.json({
-    name: app.get('pkg').name,
-    description: app.get('pkg').description,
-    version: app.get('pkg').version
+import indexRoutes from "./routes/index.routes.js";
+import productRoutes from "./routes/products.routes.js";
+import usersRoutes from "./routes/users.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+
+const app = express();
+
+//Settings
+
+app.set("port", process.env.PORT || 4000);
+app.set("json spaces", 4);
+
+//Middlewares
+app.use(
+  cors({
+    // origin: "http://localhost:3000",
   })
-})
+);
+app.use(helmet());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-get.use(require('/products',productsRoutes));
+//Routes
 
+app.use("/api", indexRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/auth", authRoutes);
 
 export default app;
-
