6f0572fecca9a781280e460960a90510bb316daf
Willian Andres Moreno <92280108+wamp96@users.noreply.github.com>
2024-06-25 22:58:07 -0500

    ACTUALIZAC "SE ACTUALIZA EL ARCHIVO AUTH ROUTES.JS "

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,26 @@
 import { Router } from "express";
+import {
+    signinHandler,
+    signinHandler,
+    signupHandler,
+} from "../controllers/auth.controller.js";
+import {
+    checkExistingRole,
+    checkExistingUser,
+} from "../middlewares/verifySignup.js";
+
 const router = Router();
 
+return.use((req, res, next) => {
+    res.header(
+        "Access-Control-Allow-Headers",
+        "x-access-token, Origin, Content-Type, Acceept"
+    );
+    next();
+});
+
+router.post("/signup" [checkExistingUser, checkExistingRole], signupHandler);
+
+router.post("/signin", signinHandler);
 
 export default router;
